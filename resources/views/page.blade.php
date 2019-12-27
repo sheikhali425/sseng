de8daf0c6688d120eb53a8af00d49d6b207731dd
komal-alt <komalsaleem07@gmail.com>
2019-12-27 14:59:26 +0500

    changes in page

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,27 +1,144 @@
 @extends('layout')
-    @section('content') 
-    <div class="detail">
-				<h4>United Bank Limited<i></i><span>Lahore</span></h4>
-				<ul class="ind_ul_margin">
-					<li>
-						<i class="fa fa-user"></i>
-							<strong>EQUIPMENT</strong>
-							<em></em>
-						<span>WC Chillers, CT, AHUs &amp; FCUs</span>
-					</li>
-					<li>
-						<i class="fa fa-map-marker"></i>
-							<strong>SERVICE</strong>
-							<em></em>
-						<span>Supply &amp; Installtion of HVAC System</span>
-					</li>
-					<li>
-						<i class="fa fa-usd"></i>
-							<strong>CAPACITY</strong>
-							<em></em>
-						<span>850 TR</span>
-					</li>
-				</ul>
-				<a href="http://mecatech.com.pk/contact/" target="_blank"><i class="icon-pdf-file"></i> CONTACT US</a>
+	
+	@section('content')
+	<style>
+.lol{
+	margin-left:-7px; important!
+}
+.vl {
+  border-left: 6px solid skyblue;
+  height: 30px;
+}
+.lop{
+	margin-left:170px;
+}
+
+		</style>
+	
+    <section class="hero-wrap hero-wrap-2" style="background-image: url('images/bu.jpg');">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-center justify-content-center">
+          <div class="col-md-9 ftco-animate text-center">
+            <h1 class="mb-2 bread">Projects</h1>
+            <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Projects <i class="ion-ios-arrow-forward"></i></span></p>
+          </div>
+        </div>
+      </div>
+    </section>
+<br>
+<br>
+<section class="ftco-section ftco-no-pt">
+    	<div class="container">
+    		<div class="row justify-content-center mb-5">
+          <div class="col-md-8 text-center heading-section ftco-animate">
+          	<span class="subheading">Project</span>
+            <h2 class="mb-4">Centaurus Mall Islamabad</h2>
+          </div>
+		</div>
+		
+  			<div class="row tabulation mt-4 ftco-animate">
+				
+  				<div class="col-md-4">
+					  <br>
+					  <br>
+					  <br>
+						<ul class="nav nav-pills  d-md-flex d-block flex-column">
+						  <li class="nav-item text-left">
+						  <i class="fa fa-spinner"></i>
+				<strong>EQUIPMENT</strong>
+				<em>-</em>
+				<br>
+			<span>WC Chillers, CT, AHUs &amp; FCUs</span>
+						  </li>
+						  <li class="nav-item text-left">
+
+						  <i class="fa fa-cogs"></i>
+				<strong>SERVICE</strong>
+				<em>-</em>
+				<br>
+			<span>Supply &amp; Installtion of HVAC System</span>
+						      </li>
+						  <li class="nav-item text-left">
+						  <i class="fa fa-tachometer"></i>
+				<strong>CAPACITY</strong>
+				<em>-</em>
+				<br>
+			<span>850 TR</span>	
+						</li>
+						  </ul>
+						  <br>
+						  <div class="lol">
+						  <div class="button">
+        
+        <button class="button button1 button3"><span><i class="fa fa-address-book" aria-hidden="true"></i>&nbsp Contact Us</span></button>
+</div>
+        </div>
+		    </div>
+					<div class="col-md-8">
+						<div class="tab-content">
+						  <div class="tab-pane container p-0 active" id="services-1">
+							  <div ko>
+							  <div class="img" style="background-image: url(images/cen.jpg);  opacity:0.8;"></div>
+			  </div>
+			                  <div class="row">
+							  <h3><a href="#"></a>Islamabad</h3>&nbsp &nbsp
+							  <div class="vl">
+
+							  &nbsp &nbsp</div><h3><a href="#"></a>Centaurus Mall</h3>
+</div>
+						   </div>
+						  
+						</div>
+					</div>
+				</div>
+    	</div>
+	</section>
+	
+    <section class="ftco-section">
+	<div class="lop">
+	<h4> <b>Features of this project:</b></h4>
+</div>
+<br>
+<br>
+			<div class="container">
+				<div class="row">
+					
+					<div class="col-lg-4 d-flex">
+						<div class="services-2 border rounded text-center ftco-animate">
+							<div class="icon mt-2 d-flex justify-content-center align-items-center"><span class="fa fa-check-square"></span></div>
+							<div class="text media-body">
+								<h3>To meet specific design requirements</h3>
+								</div>
+						</div>
+					</div>
+					<div class="col-lg-4 d-flex">
+						<div class="services-2 border rounded text-center ftco-animate">
+							<div class="icon mt-2 d-flex justify-content-center align-items-center"><span class="fa fa-check-square"></span></div>
+							<div class="text media-body">
+								<h3>Eco friendly materials</h3>
+								</div>
+						</div>
+					</div>
+					<div class="col-lg-4 d-flex">
+						<div class="services-2 border rounded text-center ftco-animate">
+							<div class="icon mt-2 d-flex justify-content-center align-items-center"><span class="fa fa-check-square"></span></div>
+							<div class="text media-body">
+								<h3>On time delivery</h3>
+									</div>
+						</div>
+					</div>
+					<div class="col-lg-4 d-flex">
+						<div class="services-2 border rounded text-center ftco-animate">
+							<div class="icon mt-2 d-flex justify-content-center align-items-center"><span class="fa fa-check-square"></span></div>
+							<div class="text media-body">
+								<h3>Quality after sales service</h3>
+									</div>
+						</div>
+					</div>
+					
+				</div>
 			</div>
-    @endsection
+		</section>
+
+@endsection
