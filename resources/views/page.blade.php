ad18fcfb101eadc52247c506dd2d09c017a802d4
komal-alt <komalsaleem07@gmail.com>
2019-12-27 12:57:30 +0500

    erroe severe

---
+++ resources/views/page.blade.php
@@ -0,0 +1,27 @@
+@extends('layout')
+    @section('content') 
+    <div class="detail">
+				<h4>United Bank Limited<i></i><span>Lahore</span></h4>
+				<ul class="ind_ul_margin">
+					<li>
+						<i class="fa fa-user"></i>
+							<strong>EQUIPMENT</strong>
+							<em></em>
+						<span>WC Chillers, CT, AHUs &amp; FCUs</span>
+					</li>
+					<li>
+						<i class="fa fa-map-marker"></i>
+							<strong>SERVICE</strong>
+							<em></em>
+						<span>Supply &amp; Installtion of HVAC System</span>
+					</li>
+					<li>
+						<i class="fa fa-usd"></i>
+							<strong>CAPACITY</strong>
+							<em></em>
+						<span>850 TR</span>
+					</li>
+				</ul>
+				<a href="http://mecatech.com.pk/contact/" target="_blank"><i class="icon-pdf-file"></i> CONTACT US</a>
+			</div>
+    @endsection
